588b5f9d6bf76d08cf0c468b4464fb7b739c8aa3
Andre-Weber <andreew@al.insper.edu.br>
2020-11-20 00:23:57 -0300

    att django

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -74,17 +74,6 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'tasks',
-        'USER': 'cloud',
-        'PASSWORD': 'cloud',
-        'HOST': 'node1',
-        'PORT': '5432',
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
